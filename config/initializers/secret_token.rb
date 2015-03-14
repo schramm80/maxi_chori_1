826f2b797511c29fc6a3051a1fa7123293829151
Schramm80 <carlos.schramm80@gmail.com>
2015-03-14 17:03:01 +0100

    Added first maxichori project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+MaxiChori1::Application.config.secret_token = '62d3dbac6585b157998acc5abe5bb583cdef843a19cd6469881a40445fa267fd632b218534fcb601e35cf36b466513a58bbcc3ff970ca29bd2f1de108a167d2b'
